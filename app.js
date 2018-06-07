ceab370d13daa6f2a247a81366fce339be222ebd
haidi20 <nhaidii@yahoo.com>
2018-06-07 22:42:00 +0800

    loop value yang menggunakan class title

---
+++ app.js
@@ -0,0 +1,7 @@
+const titles    = document.getElementsByClassName('title');
+
+console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function(title) {
+  console.log(title);
+});
