6a5a8f2e9c436f91e1ab3861c320228e11ffac14
haidi20 <nhaidii@yahoo.com>
2018-06-19 12:17:08 +0800

    update delete li

--- app.js
+++ app.js
@@ -1,20 +1,9 @@
-const listItems = document.querySelectorAll('#book-list ul li');
-Array.from(listItems).forEach(function(item) {
-  item.addEventListener('click', (e) => {
+const list = document.querySelector('#book-list ul');
 
+list.addEventListener('click' , (e) => {
+  if (e.target.className == 'delete') {
     const li = e.target.parentElement;
-    console.log('child element to remove:', li);
-    console.log('parent element to remove child from:', li.parentElement);
+    console.log(li);
     li.parentNode.removeChild(li);
-
-  });
-});
-
-// prevent default behaviour
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log('Navigation to', e.target.textContent, 'was prevented');
+  }
 });
