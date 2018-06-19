17a8d5bdd199358a019ea5f7985032f53a27813f
haidi20 <nhaidii@yahoo.com>
2018-06-19 13:10:03 +0800

    update add book

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const list = document.querySelector('#book-list ul');
 
-list.addEventListener('click' , (e) => {
-  if (e.target.className == 'delete') {
+// delete books
+
+list.addEventListener('click', (e) => {
+  if (e.target.className = 'delete') {
     const li = e.target.parentElement;
-    console.log(li);
     li.parentNode.removeChild(li);
   }
+});
+
+const forms = document.forms;
+console.log(forms);
+console.log(forms['add-book']);
+
+Array.from(forms).forEach(function(form){
+  console.log(form);
+});
+
+const addForm = forms['add-book'];
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
 });
