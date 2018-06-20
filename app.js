e262a522ef67a2750b036c5890a6c6e6e9a54b30
haidi20 <nhaidii@yahoo.com>
2018-06-20 19:32:17 +0800

    OOJ 6

--- app.js
+++ app.js
@@ -1,41 +1,14 @@
-class User {
-  constructor(email,name){
-    this.email  = email;
-    this.name   = name;
-    this.score  = 0;
-  }
-
-  login(){
-    console.log(this.email, 'just logged in');
-    return this;
-  }
-
-  logout(){
-    console.log(this.name, 'just logged out');
-    return this;
-  }
-
-  updateScore(){
-    this.score++;
-    console.log(this.email, 'score is now' , this.score);
-    return this;
-  }
+function User(email, name){
+  this.email  = email;
+  this.name   = name;
+  this.online = false;
+  this.login  = function(){
+    console.log(this.email, 'has logged in');
+  };
 }
 
-class Admin extends User {
-  deleteUser(user){
-    users = users.filter(u => {
-      return u.email != user.email
-    });
-  }
-}
+var userOne   = new User('haidi@gmail.com','haidi');
+var userTwo   = new User('andi@gmail.com','andi');
 
-var userOne = new User('haidi@gmail.com','haidi');
-var userTwo = new User('andi@gmail.com','andi');
-var admin   = new Admin('keren@gmail.com','keren');
-
-var users = [userOne, userTwo, admin];
-
-userTwo.deleteUser(userOne); // won't work
-
-console.log(users);
+console.log(userOne);
+userTwo.login();
