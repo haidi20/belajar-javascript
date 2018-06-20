bdecbb9aadd7f8df70dbdcfb72e46620747e42ea
haidi20 <nhaidii@yahoo.com>
2018-06-20 19:41:09 +0800

    OOJ 7

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ function User(email, name){
   this.email  = email;
   this.name   = name;
   this.online = false;
-  this.login  = function(){
-    console.log(this.email, 'has logged in');
-  };
 }
 
-var userOne   = new User('haidi@gmail.com','haidi');
-var userTwo   = new User('andi@gmail.com','andi');
+User.prototype.login = function(){
+  this.online = true;
+  console.log(this.email, 'has logged in');
+};
+
+User.prototype.logout = function (){
+  this.online = false;
+  console.log(this.email, 'has logged out');
+};
+
+var userOne = new User('haidi@gmail.com','haidi');
+var userTwo = new User('andi@gmail.com','andi');
 
 console.log(userOne);
 userTwo.login();
