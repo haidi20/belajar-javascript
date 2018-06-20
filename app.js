5f57e1faaf8dfe2e5a0bd8b9401992912c3c064d
haidi20 <nhaidii@yahoo.com>
2018-06-20 18:51:36 +0800

    OOJ 2

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ var userOne = {
   email: 'haidi@gmail.com',
   name: 'haidi',
   login(){
-    console.log(this.email,'has logged in');
+    console.log(this.email, 'has logged in');
   },
   logout(){
-    console.log(this.email,'has logged out');
+    console.log(this.email, 'has logged out');
   }
 };
 
+var prop = 'name'
+
+userOne.age = 25;
+userOne.logInfo = function () {
+  console.log(this.name, this.age, this.email);
+}
+
+userOne.logInfo();
 userOne.login();
 userOne.logout();
